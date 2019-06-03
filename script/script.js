2bdb4745a77420ee8242d3aa934ef48ac0bba1ce
RafSwiggers <rafswiggers@gmail.com>
2019-06-03 11:12:20 +0200

    made a functioning time display

--- script/script.js
+++ script/script.js
@@ -1,13 +1,23 @@
+var closingtime = true;
+
 function sign() {
     var now = new Date();
     var theday = now.getDay();
     var thetime = now.getHours();
     if (theday != 0 && theday != 6 && 9 <= thetime <= 17) {
         document.getElementById("openclose").innerHTML = "We're open! Come on in!";
-        console.log('it works up to the first if')
     } else if (theday = 6 && 9 <= thetime <= 16) {
         document.getElementById("openclose").innerHTML = "We're open! Come on in!";
     } else {
         document.getElementById("openclose").innerHTML = "We're closed, I'm Sorry. Come back soon!";
     }
 }
+
+function closetime() {
+    if (closingtime) {
+        closeingtime = false;
+    } else {
+        closingtime = true;
+    }
+}
+console.log(closingtime);
